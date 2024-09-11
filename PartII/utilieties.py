1ecfa212af1b823975dcae93c9a1e659e901b62e
Saúl Yaguana <saulyaguanacruz@gmail.com>
2024-09-11 14:39:39 -0500

    Building some classes with all learned in chaper II

---
+++ PartII/utilieties.py
@@ -0,0 +1,36 @@
+class Student:
+    def __init__(self, name, email_address, current_grade, school, age):
+        self.name = name
+        self.email_address = email_address
+        self.current_grade = current_grade
+        self.school = school
+        self.age = age
+        
+class Player:
+    def __init__(self, name, points, health, location):
+        self.name = name
+        self.points = ponts
+        self.health = health
+        self.location = location
+        
+    def win(self, reach_points):
+        if self.points >= reach_points:
+            return True
+        return False
+    
+    def lose(self, reach_points):
+        if self.points < reach_points or self.health <= 0:
+            return True
+        return False
+    
+    def find_player(self, location):
+        if self.location["x"] == location["x"] and self.location["y"] == location["y"]:
+            return True
+        return False
+    
+class Person:
+    def __init__(self, name, address, phone_number, birthday):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.birthday = birthday
