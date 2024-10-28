a5ab78b435fb00253ac6b794a98bb98599a65d26
Saúl Yaguana <saulyaguanacruz@gmail.com>
2024-10-28 13:48:20 -0500

    Extending the test program to create a third account interactively

--- PartII/test_code/main_bank_version1.py
+++ PartII/test_code/main_bank_version1.py
@@ -24,3 +24,20 @@ o_marys_account.deposit(100, "MarysPassword")
 # Show the accounts
 o_joes_account.show()
 o_marys_account.show()
+
+# Create another account with information from the user
+print()
+user_name = input("What is the name for the new user account?: ")
+user_balance = int(input("What is the starting balance for this account?: "))
+user_password = input("What is the password you want to use for this account?: ")
+
+o_new_account = Account(user_name, user_balance, user_password)
+
+# Show the newly created account
+o_new_account.show()
+
+# Let's deposit 100 into the new account
+o_new_account.deposit(100, user_password)
+users_balance = o_new_account.get_balance(user_password)
+print()
+print(f"After depositing 100, the user's balance is: {users_balance} - {o_new_account.balance}")
