c353cec78934f3b8f2c96ee5ee72b4a0ff16fa25
Saúl Yaguana <saulyaguanacruz@gmail.com>
2025-02-10 20:31:41 -0500

    Randomize the position of the ball image

--- PartII/PyGame/py_template.py
+++ PartII/PyGame/py_template.py
@@ -3,12 +3,16 @@ from pathlib import Path
 import pygame
 from pygame.locals import *
 import sys
+import random
 
 # 2 - Define constants
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
+BALL_WIDTH_HEIGHT = 100
+MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
+MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
 BASE_PATH = Path(__file__).resolve().parent
 
 # build a path to the file in the images folder
@@ -21,7 +25,11 @@ clock = pygame.time.Clock()
 
 # 4- Load assets: image(s), sound(s), etc.
 ball_image = pygame.image.load(path_to_ball)
+
 # 5- Initialize variables
+ball_x = random.randrange(MAX_WIDTH)
+ball_y = random.randrange(MAX_HEIGHT)   
+ball_rect = pygame.Rect(ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
 
 # 6 - Loop forever
 while True:
@@ -33,6 +41,17 @@ while True:
             pygame.quit()
             sys.exit()
             
+        # See if user clicked
+        if event.type == pygame.MOUSEBUTTONUP:
+            # mouse_x, mouse_y = event.pos  # Could do this if we needed it
+            
+            # Check if the click was in the rect of the ball
+            # If so, choose a random new location
+            if ball_rect.collidepoint(event.pos):
+                ball_x = random.randrange(MAX_WIDTH)
+                ball_y = random.randrange(MAX_HEIGHT)
+                ball_rect = pygame.Rect(ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+            
     # 8 - Do any "per frame" actions
     
     # 9 - Clear the window
@@ -40,7 +59,7 @@ while True:
     
     # 10 - Draw all window elements
     # draw ball at position 1-- across (x) and 200 down (y)
-    window.blit(ball_image, (100, 200))
+    window.blit(ball_image, (ball_x, ball_y))
     
     # 11 - Update the window
     pygame.display.update()
